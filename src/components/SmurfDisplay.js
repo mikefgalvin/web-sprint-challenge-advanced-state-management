5d619bb552784c70b22226c6928d9987c36bdeea
Mike <mgalvin@tribevr.io>
2021-01-22 13:51:21 -0600

    mvp done

--- src/components/SmurfDisplay.js
+++ src/components/SmurfDisplay.js
@@ -1,14 +1,58 @@
 import React from 'react';
+import { useSelector, useDispatch, connect } from 'react-redux';
+import { getCharacters } from '../actions/index';
+import Smurf from './Smurf';
 
 export class SmurfDisplay extends React.Component {
+
+    componentDidMount() {
+       this.props.getCharacters();
+       console.log('component mounted')
+    }
+    
+    
+
     render() {
-        return(<div>
-            
-        </div>)
+        console.log('se fetch',this.props.isFetching);
+
+        if(this.props.isFetching === true) {
+                  return (
+                    <h2>FETCHINNNNNGGGGGG</h2>
+                  )
+                }
+        else if(this.props.smurfs.length > 0) {
+            return(
+            <div>
+                {
+                    this.props.smurfs.map(smurf => (
+                        <Smurf smurf={smurf} key={smurf.id}/>
+                    ))
+                }
+            </div>
+            )}
+        else return(
+            <>
+            </>
+        )
     }
 }
 
-export default SmurfDisplay;
+const mapStateToProps = (state) => {
+    // we now have access to the WHOLE redux store!
+    // you can do any JS stuff here 
+    const memberCount = state.smurfs.length;
+  return {
+    areYouThere: true, // we can add anything to props from right here
+    smurfs: state.smurfs,
+    memberCount: memberCount,
+    isFetching: state.isFetching,
+    error: state.error,// we can add something to props from the Redux store
+  }
+  }
+
+
+export default connect(mapStateToProps, { getCharacters })(SmurfDisplay); 
+
 
 //Task List:
 //1. Import in all needed components and library methods.
